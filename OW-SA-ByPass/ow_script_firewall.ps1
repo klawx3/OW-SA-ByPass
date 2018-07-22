fd4b928a88fe3314b7a3f6f58f7927d205d43ac2
ruyo <gunztitans@gmail.com>
2018-07-22 19:59:57 -0400

    iconos arreglados
    iconos arreglados

--- OW-SA-ByPass/ow_script_firewall.ps1
+++ OW-SA-ByPass/ow_script_firewall.ps1
@@ -53,10 +53,10 @@ function Create-ShorcutWithArg($shorcut_name, $ss){
     $current_path = $(Get-Location).Path
     $Shortcut.Arguments = [string]$ss
     $Shortcut.TargetPath = $current_path + "\" + $bat_name_file
-    if ($ss -like "true" -or $ss -eq "0"){
+    if ($ss -like "true" -or $ss -eq "1"){
     $Shortcut.IconLocation = $current_path + "\" + "icon\_na.ico"
     }
-    if ($ss -like "true" -or $ss -eq "1"){
+    if ($ss -like "true" -or $ss -eq "0"){
     $Shortcut.IconLocation = $current_path + "\" + "icon\_las.ico"
     }
     $Shortcut.Save()
@@ -86,8 +86,8 @@ if($args.Length -eq 0){ # without args - create firewall rule
         exit
     }
     Write-Output "Creando accesos directos para habilitar y desabilitar la regla de firewall"
-    Create-ShorcutWithArg "OW - Habilitar NA" "0"
-    Create-ShorcutWithArg "OW - Habilitar LAS" "1"
+    Create-ShorcutWithArg "OW - Habilitar NA" "1"
+    Create-ShorcutWithArg "OW - Habilitar LAS" "0"
     Write-Output "Cambios creados con exito!"
 } else { #with args
     $enable = $args[0]
